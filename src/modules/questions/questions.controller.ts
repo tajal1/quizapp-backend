e7f7ab20cc671ca5fc79757c63b98a9b7efc1e32
TajalTechnology <mrpresident1189@gmail.com>
2024-06-24 02:54:02 +0600

    Code format

--- src/modules/questions/questions.controller.ts
+++ src/modules/questions/questions.controller.ts
@@ -1,24 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common'
+import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common'
 import { QuestionsService } from './questions.service'
 import { CreateQuestionDto } from './dto/create-question.dto'
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
 import { AuthGuard } from '../auth/auth.guard'
 
 @ApiTags('Questions')
+@ApiBearerAuth('JWT')
+@UseGuards(AuthGuard)
 @Controller('questions')
 export class QuestionsController {
     constructor(private readonly questionsService: QuestionsService) {}
 
-    @ApiBearerAuth('JWT')
-    @UseGuards(AuthGuard)
     @Post()
     create(@Body() createQuestionDto: CreateQuestionDto, @Req() req: any) {
         const createQuestionDtoWithUserId = { ...createQuestionDto, user_id: req.user?._id }
         return this.questionsService.create(createQuestionDtoWithUserId)
     }
 
-    @ApiBearerAuth('JWT')
-    @UseGuards(AuthGuard)
     @Get()
     findAll() {
         return this.questionsService.findAll()
--- src/modules/quizes/quizes.controller.ts
+++ src/modules/quizes/quizes.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Put } from '@nestjs/common'
+import { Controller, Get, Post, Body, Patch, Param, UseGuards, Req, Put } from '@nestjs/common'
 import { QuizesService } from './quizes.service'
 import { CreateQuizeDto } from './dto/create-quize.dto'
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
@@ -7,6 +7,8 @@ import { SubmitQuizDto } from './dto/submit-quize.dto'
 import { UsersService } from '../users/users.service'
 
 @ApiTags('Quizes')
+@ApiBearerAuth('JWT')
+@UseGuards(AuthGuard)
 @Controller('quizes')
 export class QuizesController {
     constructor(
@@ -14,37 +16,27 @@ export class QuizesController {
         private readonly usersService: UsersService
     ) {}
 
-    @ApiBearerAuth('JWT')
-    @UseGuards(AuthGuard)
     @Post()
     create(@Body() createQuizeDto: CreateQuizeDto, @Req() req: any) {
         const createQuizeDtoWithUserId = { ...createQuizeDto, user_id: req.user?._id }
         return this.quizesService.create(createQuizeDtoWithUserId)
     }
 
-    @ApiBearerAuth('JWT')
-    @UseGuards(AuthGuard)
     @Get()
     findAll(@Req() req: any) {
         return this.quizesService.findAll(req.user?._id)
     }
 
-    @ApiBearerAuth('JWT')
-    @UseGuards(AuthGuard)
     @Get(':_id')
     findOne(@Param('_id') _id: string) {
         return this.quizesService.findOne(_id)
     }
 
-    @ApiBearerAuth('JWT')
-    @UseGuards(AuthGuard)
     @Patch('/:quiz_id/subject/:subject_id')
     quizStartBySubjectId(@Param('quiz_id') quiz_id: string, @Param('subject_id') subject_id: string, @Req() req: any) {
         return this.quizesService.quizStartBySubjectId(quiz_id, subject_id, req.user?._id)
     }
 
-    @ApiBearerAuth('JWT')
-    @UseGuards(AuthGuard)
     @Put(':_id/subject/:subject_id')
     submitQuizBySubjectId(
         @Param('_id') _id: string,
@@ -55,12 +47,9 @@ export class QuizesController {
         return this.quizesService.submitQuizBySubjectId(_id, subject_id, submitQuizDto, req.user?._id)
     }
 
-    @ApiBearerAuth('JWT')
-    @UseGuards(AuthGuard)
     @Patch('score/:quiz_id')
     async score(@Param('quiz_id') quiz_id: string, @Req() req: any) {
         const score = await this.quizesService.score(quiz_id, req.user?._id)
-        console.log(score)
         await this.usersService.userUpdateById(req.user?._id, {
             positive_score: score.total_positive_score,
             negetive_score: score.total_negative_score
