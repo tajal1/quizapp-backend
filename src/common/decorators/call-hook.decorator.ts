3c8d90ecb7d4db57e87c9d89b337118be29434c4
Tajal <admin@admins-MacBook-Pro-3.local>
2025-04-23 02:32:27 +0600

    dynamic business logic execute

--- src/common/decorators/call-hook.decorator.ts
+++ src/common/decorators/call-hook.decorator.ts
@@ -1,3 +1,6 @@
+import { Users } from "src/domain/v1/users";
+import { join } from 'path';
+
 // call-hook.decorator.ts
 export function CallHook() {
     return function (
@@ -11,12 +14,32 @@ export function CallHook() {
 
         descriptor.value = async function (...args: any[]) {
             const req = this?.request;
-            console.log('🎯 request in decorator:', req?.url, req?.body);
 
-            console.log('=============', target, propertyKey, ...args)
-            console.log(`[CallHook] Before ${propertyKey}`);
+
+
+            const url = req.url; // e.g., /api/v1/users
+            const pathModule = url.replace('/api', 'domain') + '.ts';
+            const newUsers = new Users()
+
+            const modulePath = pathModule.replace(/\//g, '/'); // normalize slashes
+            const pathToModule = join(__dirname, '../../domain/v1/users');
+            // Dynamically import the module
+            const logic = await import(pathToModule);
+
+            // Access the exported class
+            const UsersClass = logic.Users;
+
+            // Create an instance and call method
+            const instance = new UsersClass();
+            const result = instance.neame();
+            return { result }
+
+            console.log(result); // 👉 'My name is tajal'
+
+            // console.log('=============', target, propertyKey, ...args)
+            // console.log(`[CallHook] Before ${propertyKey}`);
             // const result = await originalMethod.apply(this, args);
-            console.log(`[CallHook] After ${propertyKey}`);
+            // console.log(`[CallHook] After ${propertyKey}`);
             // return result;
         };
 
---
+++ src/domain/v1/users.ts
@@ -0,0 +1,6 @@
+export class Users {
+    neame() {
+        console.log('Hello')
+        return 'My name is tajal'
+    }
+}
